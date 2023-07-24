03c475c3833bfe00fce67eca6fd51d4b1ab4bd8d
Bekbol1 <kbekbol9@gmail.com>
2023-07-24 15:17:09 +0600

    Lesson 2 1) Indicator 2) Data for viewpager

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.taskmanager
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -22,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController.navigate(R.id.onBoardingFragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -37,5 +41,15 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.onBoardingFragment) {
+                navView.isVisible = false
+                supportActionBar?.hide()
+            } else {
+                navView.isVisible = true
+                supportActionBar?.show()
+            }
+        }
     }
-}
+    }
---
+++ app/src/main/java/com/example/taskmanager/ui/onboarding/adapter/OnBoardingAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.taskmanager.ui.onboarding.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.taskmanager.databinding.ItemOnboardingBinding
+import com.example.taskmanager.model.OnBoarding
+
+class OnBoardingAdapter(private val onClick: () -> Unit) :
+    Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
+
+    private val data = arrayListOf<OnBoarding>(
+        OnBoarding("Reflect","Reflect your success with Task Manager!","https://uploads-ssl.webflow.com/6156c92cb3f1a26201745fa2/621b22f337c639bd78db0963_6197773c733ba15f7b3da1da_20943650-min.jpg"),
+        OnBoarding("Track Progress","You can easily track your daily progress!","https://img.freepik.com/free-vector/hand-drawn-business-planning-with-task-list_23-2149164275.jpg"),
+        OnBoarding("Perform","Perform your tasks efficiently!","https://miro.medium.com/max/1400/1*8ygFKYb0Yo6Hc-vnScGA9A.png")
+    )
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
+        return OnBoardingViewHolder(
+            (ItemOnboardingBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ))
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: OnBoardingViewHolder, position: Int) {
+        holder.bind(data.get(position))
+    }
+
+    inner class OnBoardingViewHolder(private val binding: ItemOnboardingBinding) :
+        ViewHolder(binding.root) {
+        fun bind(onBoarding: OnBoarding) {
+            binding.tvTitle.text = onBoarding.title
+            binding.tvDesc.text = onBoarding.desc
+            Glide.with(binding.ivBoard).load(onBoarding.image).into(binding.ivBoard)
+
+            binding.btnStart.isVisible = adapterPosition == data.lastIndex
+            binding.skip.isVisible = adapterPosition != data.lastIndex
+
+
+            binding.btnStart.setOnClickListener {
+                onClick()
+            }
+            binding.skip.setOnClickListener {
+                onClick()
+            }
+        }
+    }
+
+}
